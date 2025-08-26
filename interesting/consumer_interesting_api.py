52d47cb8702f30585e24c0e465017cb61cd62617
shuki <shukiheftler@gmail.com>
2025-08-26 14:58:17 +0300

    day 2

---
+++ interesting/consumer_interesting_api.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+from app.manage_mongo import MongoManger
+import uvicorn
+
+app = FastAPI()
+
+consumer = MongoManger("interesting")
+
+@app.get("/consume")
+def consume():
+    consumer.write("interesting")
+    return {"status":"ok","consume":"interesting"}
+
+@app.get("/data")
+def get_last_data(read = "all"):
+    if read == "all":
+        return consumer.read_all()
+    return consumer.read_last()
+
+if __name__ == '__main__':
+    uvicorn.run("consumer_interesting_api:app", host="127.0.0.1", port=8001, reload=True)
